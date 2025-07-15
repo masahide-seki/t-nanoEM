d5192badea8b65bd9739bdb2762097703c02314b
masahide-seki <110798863+masahide-seki@users.noreply.github.com>
2025-07-15 16:34:30 +0900

    Add files via upload

---
+++ t-nanoEM_paper/Others/Scripts for methylation heatmaps/main.R
@@ -0,0 +1,14 @@
+
+# --- 0. Load Required Packages ----------------------
+
+library(data.table)
+library(openxlsx)
+library(gplots)
+
+# --- 1. Run scripts ---------------------------------
+
+source("./scripts/Figure4D.R")
+source("./scripts/Figure4F.R")
+source("./scripts/Figure4H.R")
+source("./scripts/Figure5D.R")
+source("./scripts/Figure5F.R")
